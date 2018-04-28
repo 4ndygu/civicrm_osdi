1fc4a18fad25950943b4d8a9816214c32ba3ab74
Andy Gu <andygu@usc.edu>
2018-04-27 18:58:09 -0700

    seed abstract class

---
+++ AbstractContactImporter.php
@@ -0,0 +1,15 @@
+<?php
+abstract class AbstractContactImporter
+{
+
+    // endpoint
+    private $endpoint;
+    private $schema;
+
+    abstract public function pull_endpoint_data();
+    abstract public function validate_endpoint_data($data);
+    abstract public function format_data($data);
+    abstract public function store_data($data);
+}
+?>
+
