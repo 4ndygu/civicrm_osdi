e491c3961615af1cd8e586a29fce61cfeb2d9069
Andy Gu <andygu@usc.edu>
2018-04-27 23:48:01 -0700

    [poc] interface inherited class with php hal explorer

--- ActionNetworkContactImporter.php
+++ ActionNetworkContactImporter.php
@@ -5,11 +5,33 @@ require_once("AbstractContactImporter.php");
 class ActionNetworkContactImporter extends AbstractContactImporter
 {
 
+    private $client;
+    private $explorer;
+    private $adapter;
+
     // constructor
     public function __construct($endpoint, $schema, $apikey) {
         self.endpoint = $endpoint;
         self.schema = $schema;
         self.apikey = $apikey;
+
+        // demo api key is c173bee8d3238bb5bfc4dd014f207feb.
+        // TODO: throw $apikey into some config file.
+
+        self.client = new \GuzzleHttp\Client();
+        self.explorer = new \HalExplorer\Explorer();
+        self.adapter = new \HalExplorer\ClientAdapters\Adapter();
+
+        self.adapter->setClient($client);
+        self.explorer->setAdapter($adapter)->setBaseUrl(self.endpoint);
+
+        // override default values for the headers
+        self.explorer->setDefaults(function($original){
+            $original["headers"]["Accept"] = "application/hal+json, application/json";
+            $original["headers"]["OSDI-API-Token"] = self.apikey;
+
+            return $original;
+        });
     }
 
     public function pull_endpoint_data() {
