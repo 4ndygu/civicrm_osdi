540596100b4b62f4fd7e016c33f88eae718b944f
Andy Gu <andygu@usc.edu>
2018-05-14 13:51:53 -0700

    queue works

--- CRM/OSDIQueue/Tasks.php
+++ CRM/OSDIQueue/Tasks.php
@@ -5,20 +5,18 @@ class CRM_OSDIQueue_Tasks {
 		// this expects a hal object that represents a page of contacts
 		// where do u load an action?
 		CRM_Core_Session::setStatus('executing add contact task', 'Queue task', 'success');
-		//$properties = $contact["email_addresses"];
-		/*try {
-			$properties = $contact->getProperties();
+		try {
 			$result = civicrm_api3('Contact', 'create', array(
-				'first_name' => $properties["given_name"],
-				'last_name' => $properties["family_name"],
-				'email' => $properties["email_addresses"][0]["address"],
-				'display_name' => $properties["family_name"],
+				'first_name' => $contact["given_name"],
+				'last_name' => $contact["family_name"],
+				'email' => $contact["email_addresses"][0]["address"],
+				'display_name' => $contact["family_name"],
 				'contact_type' => 'Individual'
 			));
 		}
 		catch (Exception $e) {
 			return False;
-		}*/
+		}
 
 		return True;
 	}
