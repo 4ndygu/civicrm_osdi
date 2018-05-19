6ba97ad4951fb8583a0e0081c65fee7d8fe15f99
Andy Gu <andygu@usc.edu>
2018-05-18 17:46:55 -0700

    exporter.export api init

---
+++ api/v3/Exporter/Export.php
@@ -0,0 +1,63 @@
+<?php
+use CRM_Osdi_ExtensionUtil as E;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use Hateoas\HateoasBuilder;
+
+/**
+ * Exporter.Export API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_exporter_Export_spec(&$spec) {
+  $spec['object']['api.required'] = 1;
+}
+
+/**
+ * Exporter.Export API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_exporter_Export($params) {
+    //TODO: convert to tertiary operators
+    if (!isset($params["page"])) {
+        $offset = 0;
+    } else {
+        $offset = $params["page"] * 25;
+    }
+
+    if (!isset($params["limit"])) {
+        $limit = 0;
+    } else {
+        $limit = $params["limit"];
+    }
+
+    $hateoas = HateoasBuilder::create()->build();
+
+    $result = NULL;
+    if (strtolower($params["object"]) == "contact") {
+        // dump the contacts
+        $result = civicrm_api3('Contact', 'get', array(
+            'contact_type' => "Individual",
+            'sequential' => 1,
+            'options' => array("offset" => $offset, "limit" => $limit)
+        ));
+
+        $response["properties"]["page"] = $offset / 25;
+        $response["properties"]["per_page"] = $result["count"];
+
+        $response["links"]["next"] = CRM.config.resourceBase . "extern/rest.php?entity=Exporter&action=export&api_key=userkey&key=sitekey&json=" . JSON
+    }
+
+    var_dump($result);
+
+    return civicrm_api3_create_success($response, $params, 'Exporter', 'export');
+}
