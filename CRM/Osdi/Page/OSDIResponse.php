fca87ced75c4f41d69e1772e8a89d7fe6cf7512d
Andy Gu <andygu@usc.edu>
2018-05-24 23:55:06 -0700

    permit params on crm webhook page

--- CRM/Osdi/Page/OSDIResponse.php
+++ CRM/Osdi/Page/OSDIResponse.php
@@ -5,9 +5,26 @@ class CRM_Osdi_Page_OSDIResponse extends CRM_Core_Page {
 
   public function run() {
 
-    $result = civicrm_api3('Exporter', 'export', array(
-        'object' => 'Contact'
-    ));
+    $params = array();
+
+    if (!isset($_GET["object"])) {
+        print "must set 'object' parameter";
+
+        CRM_Utils_System::civiExit();
+        parent::run();
+        return;
+    }
+
+    $params["object"] = $_GET["object"];
+
+    $optionals = array("apikey", "sitekey", "page", "limit");
+    foreach ($optionals as $optional) {
+        if (isset($_GET[$optional])) {
+            $params[$optional] = $_GET[$optional];
+        }
+    }
+    
+    $result = civicrm_api3('Exporter', 'export', $params);
 
     print json_encode($result["values"]);
 
