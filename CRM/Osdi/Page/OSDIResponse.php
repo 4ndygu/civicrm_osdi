8e768f7779c97a63083da4a3fcb247589939f0b3
Andy Gu <andygu@usc.edu>
2018-06-10 16:18:48 -0700

    framework for post requests

--- CRM/Osdi/Page/OSDIResponse.php
+++ CRM/Osdi/Page/OSDIResponse.php
@@ -7,27 +7,33 @@ class CRM_Osdi_Page_OSDIResponse extends CRM_Core_Page {
 
     $params = array();
 
-    if (!isset($_GET["object"])) {
-        print "must set 'object' parameter";
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        if (!isset($_GET["object"])) {
+            print "must set 'object' parameter";
 
-        CRM_Utils_System::civiExit();
-        parent::run();
-        return;
-    }
-
-    $params["object"] = $_GET["object"];
-
-    $optionals = array("apikey", "sitekey", "page", "limit", "id");
-    foreach ($optionals as $optional) {
-        if (isset($_GET[$optional])) {
-            $params[$optional] = $_GET[$optional];
+            CRM_Utils_System::civiExit();
+            parent::run();
+            return;
         }
-    }
-    
-    $result = civicrm_api3('Exporter', 'export', $params);
 
-    header('Content-Type: application/json');
-    print json_encode($result["values"], JSON_PRETTY_PRINT);
+        $params["object"] = $_GET["object"];
+
+        $optionals = array("apikey", "sitekey", "page", "limit", "id");
+        foreach ($optionals as $optional) {
+            if (isset($_GET[$optional])) {
+                $params[$optional] = $_GET[$optional];
+            }
+        }
+
+        $result = civicrm_api3('Exporter', 'export', $params);
+
+        header('Content-Type: application/json');
+        print json_encode($result["values"], JSON_PRETTY_PRINT);
+    } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $entityBody = json_decode(file_get_contents('php://input'));
+
+
+    }
 
     CRM_Utils_System::civiExit();
     parent::run();
