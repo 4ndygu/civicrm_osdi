cf691f5fae72fe9711dd104da060fdb4d15ebea0
Andy Gu <andygu@usc.edu>
2018-07-15 16:37:54 -0700

    skeleton for job add api

---
+++ api/v3/OSDIJob/Add.php
@@ -0,0 +1,36 @@
+<?php
+use CRM_Osdi_ExtensionUtil as E;
+
+/**
+ * OSDIJob.Add API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_o_s_d_i_job_Add_spec(&$spec) {
+  $spec['resource']['api.required'] = 1;
+  $spec['rootendpoint']['api.required'] = 1;
+  $spec['signupendpoint']['api.required'] = 1;
+  $spec['peopleendpoint']['api.required'] = 1;
+  $spec['groupid']['api.required'] = 1;
+  $spec['ruleid']['api.required'] = 1;
+  $spec['reqfields']['api.required'] = 1;
+  $spec['syncconfig']['api.required'] = 1;
+  $spec['timezone']['api.required'] = 1;
+}
+
+/**
+ * OSDIJob.Add API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_o_s_d_i_job_Add($params) {
+  $returnValues = array();
+  return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
+}
