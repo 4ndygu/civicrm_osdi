a5b6d856a0804ba8b4c4ad24e1180032e1b50ffe
Jon <1796012+MegaphoneJon@users.noreply.github.com>
2018-08-23 23:11:08 -0400

    Define setting (#15)
    * Define the setting
    
    * add title

---
+++ settings/Osdi.setting.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+  'multisite_acl_enabled' => array(
+    'group_name' => 'OSDI',
+    'group' => 'osdi',
+    'name' => 'server_time_zone',
+    'title' => 'OSDI Server Time Zone',
+    'type' => 'String', //Wish there was something to capture non-integer numbers
+    'default' => 0,
+    'add' => '5.4',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'Time zone offset for OSDI interactions',
+    'help_text' => 'Time zone offset of local server',
+  ),
+);
